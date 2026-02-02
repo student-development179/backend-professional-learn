5f4129f07bd6ed8ddb59443bc54e400384c95c4e
Rahul Dudharejiya <dudharejiya2499@gmail.com>
2026-02-02 15:41:07 +0530

    to connect database and debugging And utils code discusion

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: '.env'
+});
+import connectDB from "./db/index.js";
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port :
+        ${process.env.PORT}`);
+    })
+})
+
+.catch((error) =>{
+    console.log("MongoDB connection failed !!! ", error);
+})
+
+
+
+
+//this code are also good but we have to use more things so this code are verry heavy show and complex so this is avoid but okk 
+
+/*
+import express from "express";
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        //this are app listener
+        app.on("error",(error) =>{
+            console.log("This site is not answer your query: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,() => {
+            console.log(`App is listening on port
+                ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("ERROR: ", error)
+        throw error
+    }
+})()
+*/
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//this are also good both are same but as per compony choices....
+const asyncHandler = (RequestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(RequestHandler(req, res, next)).
+        catch((err) => next(err))
+    }}
+
+export { asyncHandler }
+
+//this are also used in data handler this are also good 
+//but some compony has differnt diff to handle data so which has compony choice which are better so 
+//which are easy was you can its try
+
+//this megafunction are
+// const asyncHandler = (fn) =>  () => {}  or  =(fn) => { => {}}
+//this are we used in replace to curly braces is a uniques in this
+
+
+/*const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success : false,
+            message : err.message
+        })
+    }
+}*/
